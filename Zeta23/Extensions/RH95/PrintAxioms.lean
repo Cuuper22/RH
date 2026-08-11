71aaf89922c682cae3a9261fc5ef7b7686a6f963
Cúper (Yousef) Anas <97895153+Cuuper22@users.noreply.github.com>
2026-08-10 18:25:33 -0700

    Add RH95 axiom audit commands

---
+++ Zeta23/Extensions/RH95/PrintAxioms.lean
@@ -0,0 +1,14 @@
+import Zeta23.Extensions.RH95
+
+#print axioms Zeta23.RH95Audit.profile85_pos
+#print axioms Zeta23.RH95Audit.J85_from_antiderivatives
+#print axioms Zeta23.RH95Audit.c85_exact
+#print axioms Zeta23.RH95Audit.proportion85_gt_seventeen_twentieths
+#print axioms Zeta23.RH95Audit.eighty_five_percent_of_rankTrace
+#print axioms Zeta23.RH95Audit.quarticP_constraints
+#print axioms Zeta23.RH95Audit.quarticP_nonpos
+#print axioms Zeta23.RH95Audit.quarticP_le_sq
+#print axioms Zeta23.RH95Audit.quarticP_le_level
+#print axioms Zeta23.RH95Audit.quarticFixed_exact
+#print axioms Zeta23.RH95Audit.quartic_fixed_point_of_scalar_inequality
+#print axioms Zeta23.RH95Audit.support143_input_implies_85
