5edfc87085cdef1bbf086c74e7a2ae60e3393788
Cúper (Yousef) Anas <97895153+Cuuper22@users.noreply.github.com>
2026-08-12 09:54:18 -0700

    Compile aligned layout bridge with quartic targets

--- RH/Zeta85/QuarticMain.lean
+++ RH/Zeta85/QuarticMain.lean
@@ -10,6 +10,7 @@ import RH.Zeta85.Discharge.IsometricKernel
 import RH.Zeta85.Discharge.BalancedMixer
 import RH.Zeta85.Discharge.VirtualChannelMixer
 import RH.Zeta85.Discharge.RepeatedChannelCompression
+import RH.Zeta85.Discharge.AlignedIsometricLayout
 import RH.Zeta85.Discharge.AlignedChannelScales
 import RH.Zeta85.Statement
 
