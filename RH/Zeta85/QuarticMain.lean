b77df213933d168a1554e8a36ef55fb26735bbd2
Cúper (Yousef) Anas <97895153+Cuuper22@users.noreply.github.com>
2026-08-12 17:00:51 -0700

    Compile annular RS diagonal through quartic target

--- RH/Zeta85/QuarticMain.lean
+++ RH/Zeta85/QuarticMain.lean
@@ -6,6 +6,7 @@ SPDX-License-Identifier: Apache-2.0
 
 import RH.Zeta85.Discharge.QuarticTransfer
 import RH.Zeta85.Discharge.RSBlockMomentBridge
+import RH.Zeta85.Discharge.RSAnnularDiagonal
 import RH.Zeta85.Discharge.IsometricBlock
 import RH.Zeta85.Discharge.IsometricKernel
 import RH.Zeta85.Discharge.BalancedMixer
